eee70ed5aa931195e4476677cee3c24f8392b21d
Danny <dannyjung23@gmail.com>
2025-09-11 20:15:52 +1200

    Create and Register Authentication Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import authentication from './authentication';
 
 const router = express.Router();
 
 // Default function to create and return a router instance
 export default (): express.Router => {
+    authentication(router);
     return router;
 };
