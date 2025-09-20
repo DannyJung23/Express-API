58b59aa80fa111c9ee9c4cb07ffaf2e369b82da5
Danny <dannyjung23@gmail.com>
2025-09-20 15:21:42 +1200

    fix: Add authentication to delete method

--- src/router/users.ts
+++ src/router/users.ts
@@ -4,5 +4,5 @@ import { isOwner, isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
-    router.delete('/users/:id', isOwner, deleteUser);
+    router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
 };
