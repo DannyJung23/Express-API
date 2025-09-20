dc3028cf8cab2d109fa03aca0d6e2a54583e5b60
Danny <dannyjung23@gmail.com>
2025-09-20 14:13:45 +1200

    refactor: Rename cookie

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -27,7 +27,7 @@ export const login = async (req: express.Request, res: express.Response) => {
 
         await user.save();
 
-        res.cookie('DANNY-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+        res.cookie('USER-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
 
         return res.status(200).json(user).end();
         
