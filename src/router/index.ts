6a9b251ea68f75610776a89f5e41f675ea4f487b
Danny <dannyjung23@gmail.com>
2025-09-20 14:48:56 +1200

    feat: Add users router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import authentication from './authentication';
+import users from './users';
 
 const router = express.Router();
 
 // Default function to create and return a router instance
 export default (): express.Router => {
     authentication(router);
+    users(router);
+    
     return router;
 };
